addc056e43b69bc7d660d92eecf06bf4701bc3d5
globefire <sahun5774@gmail.com>
2019-06-28 22:37:45 +0530

    web ss

--- firebase-test-2.js
+++ firebase-test-2.js
@@ -60,11 +60,11 @@ function mail(i){
 				To : email,
 				From : "phpmailer7.0@gmail.com",
 				Subject : "You have to work according to the details mentioned",
-				Body : "The location is " + location + ". This was reported on " + time + ". The estimated area of the pothole is " + area + ". The emergency factor is " + status + "."
+				Body : "The location is " + location + ". This was reported on " + time + ". The estimated area of the pothole is " + area + "."
 		}).then(
 			message => alert(message)
 		);
-			location.reload();
+			
 			}
 			else{
 				alert('unsuccessful')
@@ -100,7 +100,7 @@ fireDatabase.ref("Pothole_Reports/").once('value', function(snapshot){
 			if(child_childSnapshot.val().Status>=50 && child_childSnapshot.val().Status < 80){ 
 				//console.log(child_childSnapshot.val().Status+" "+childSnapshot.key+" "+child_childSnapshot.key+" "+child_childSnapshot.val().GPS_Coordinates+" "+child_childSnapshot.val().RealArea);
 				console.log(child_childSnapshot.key+" "+childSnapshot.key);
-				$("#t_body").append("<tr id="+i+"row><td>"+i+"</td><td id="+i+"stat>"+child_childSnapshot.val().Status+"</td><td id = "+i+"time>"+child_childSnapshot.key+"</td><td id="+i+"loc>"+child_childSnapshot.val().GPS_Coordinates+"</td><td id = "+i+"area>"+child_childSnapshot.val().RealArea+"</td><td><select id='"+i+"drop'><option value='sahun5774@gmail.com'>sahun5774@gmail.com</option><option value='rugvedpk2@gmail.com'>rugvedpk2@gmail.com</option><option value='santiagoreha.r@gmail.com'>santiagoreha.r@gmail.com</option><option value='anantthegreat9@gmail.com'>anantthegreat9@gmail.com</option></select></td><td scope='col'><input id = "+i+ '-eng' + " onclick='mail("+i+");test("+JSON.stringify(childSnapshot.key)+"," +JSON.stringify(child_childSnapshot.key)+");' class='btn btn-primary' type='button' name='engineer' value='engg' data-toggle='modal' data-target='#exampleModal'></td><td scope='col'><input id = "+i+'-user'+" onclick='popup("+JSON.stringify(childSnapshot.key)+"," +JSON.stringify(child_childSnapshot.key)+");' class='btn btn-primary' type='button' name='user' value='user' data-toggle='modal' data-target='#UserModal'></td></tr>");
+				$("#t_body").append("<tr id="+i+"row><td>"+i+"</td><td id="+i+"stat>"+child_childSnapshot.val().Status+"</td><td id = "+i+"time>"+child_childSnapshot.key+"</td><td id="+i+"loc>"+child_childSnapshot.val().GPS_Coordinates+"</td><td id = "+i+"area>"+child_childSnapshot.val().RealArea+"</td><td><select id='"+i+"drop'><option value='sahun5774@gmail.com'>sahun5774@gmail.com</option><option value='rugvedpk2@gmail.com'>rugvedpk2@gmail.com</option><option value='santiagoreha.r@gmail.com'>santiagoreha.r@gmail.com</option><option value='anantthegreat9@gmail.com'>anantthegreat9@gmail.com</option></select></td><td scope='col'><input id = "+i+ '-eng' + " onclick='mail("+i+");test("+JSON.stringify(childSnapshot.key)+"," +JSON.stringify(child_childSnapshot.key)+");' class='btn btn-primary' type='button' name='engineer' value='send' data-toggle='modal' data-target='#exampleModal'></td><td scope='col'><input id = "+i+'-user'+" onclick='popup("+JSON.stringify(childSnapshot.key)+"," +JSON.stringify(child_childSnapshot.key)+");' class='btn btn-primary' type='button' name='user' value='ack' data-toggle='modal' data-target='#UserModal'></td></tr>");
 
 				//$("#modal-forms").append("<div class='modal fade' id='EnggModal' tabindex='-1' role='dialog' aria-labelledby='EnggModalLabel' aria-hidden='true'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-header'><h5 class='modal-title' class='exampleModalLabel'>Send Email to Engineer</h5><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div><div class='modal-body'><form><table><tr><td>Email:</td><td><input class='form-control' type='email' id='address' name='email'></td></tr><tr><td>Details:<p class='row_data'>"+child_childSnapshot.val().Status+"<br>"+childSnapshot.key+"<br>"+child_childSnapshot.key+"<br>"+child_childSnapshot.val().GPS_Coordinates+"<br>"+child_childSnapshot.val().RealArea +"</p></td></tr></table></form></div><div class='modal-footer'><button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button><button type='button' id='submit"+i+"' class='btn btn-primary'>Send</button></div></div></div></div>");
 				hide(i);
